848c35eb6b9d8c0574007d84e7e4e1721dae31a8
siva921 <60959198+siva921@users.noreply.github.com>
2020-02-20 10:51:49 +0530

    Update WeatherForecastController.cs

--- Microservice/Controllers/WeatherForecastController.cs
+++ Microservice/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Microservice.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Scorching1"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
